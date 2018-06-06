1218efb609a1112361951483c0b432d4295729d9
Vitaly <volkondr@ya.ru>
2018-06-06 22:12:35 +0300

    ..

--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ var OFFER__TITLE = [
 ];
 var PRICE_MIN = 1000;
 var PRICE_MAX = 1000000;
-var PROPERTY_TYPES = [
+var TYPES = [
   'palace',
   'flat',
   'house',
@@ -31,9 +31,48 @@ var TIME_CHECK_IN = [
   '13:00',
   '14:00'
 ];
-
 var TIME_CHECK_OUT = [
   '12:00',
   '13:00',
   '14:00'
 ];
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+var LOCATION_X_MIN = 300;
+var LOCATION_X_MAX = 900;
+var LOCATION_Y_MIN = 130;
+var LOCATION_Y_MAX = 630;
+
+//случайное число из диапазона
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// возврат случайного элемента из массива
+var getRandomElement = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+
+var map = document.querySelector('.map');
+var mapPins = document.querySelector('.map__pins');
+var mapPin = document.querySelector('template').content.querySelector('.map__pin');
+var mapCard = document.querySelector('template').content.querySelector('.map__card');
+
+var makePin = function (offerObject) {
+
+};
+
+
+map.classList.remove('map--faded');
