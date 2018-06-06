544d4ab9b0b6e5695cd2a77c46a4204bc62e4abd
Vitaly <volkondr@ya.ru>
2018-06-06 19:32:46 +0300

    ..

---
+++ js/map.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var OFFERS_COUNT = 8;
+
+var AVATAR_LINK_PATH = 'img/avatars/user';
+var AVATAR_NAME_PREFIX = 0;
+var AVATAR_FILENAME_EXTENSION = '.png';
+
+var OFFER__TITLE = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
+var PRICE_MIN = 1000;
+var PRICE_MAX = 1000000;
+var PROPERTY_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+var ROOMS_MIN = 1;
+var ROOMS_MAX = 5;
+var TIME_CHECK_IN = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+var TIME_CHECK_OUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
